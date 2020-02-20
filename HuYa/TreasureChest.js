b6a153a89bad1d4b68c15c556222e97fc47dc76c
DTEmiemie <35299161+DTEmiemie@users.noreply.github.com>
2020-02-20 21:23:26 +0800

    Create a directory called HuYa

---
+++ HuYa/TreasureChest.js
@@ -0,0 +1,23 @@
+auto();
+
+// You should execute the following code in the live interface
+
+function GetBeans(BeanType){
+    if (BeanType === "Sliver") {
+        function ClickTreasureChest(){
+            className("FrameLayout").drawingOrder(1).depth(11).clickable().findOnce().click();
+         };
+
+         function ClickBoxGift() {
+             click("领取");
+         };
+
+         // Start to run
+        ClickTreasureChest();
+        sleep(1000);
+        ClickBoxGift();
+    };
+};
+
+GetBeans("Sliver");
+

3a56d3c014cf09306ad3b2877013f9243ab6dcd8
DTEmiemie <35299161+DTEmiemie@users.noreply.github.com>
2020-02-23 02:10:26 +0800

    Update function: 1. Determine whether you need to log in. If you need to log in, click the login button. If you are already logged in and you can click the avatar to call out the menu, then click the avatar to call out the menu, and then enter the like interface. 2.Add a new thread to listen for toast messages. If Like up to 50 nearby people every day appears, it will automatically exit the script. 3. The second way to exit the script waits for perfection.

--- TencentCardLike/test.js
+++ TencentCardLike/test.js
@@ -1,6 +1,38 @@
 auto();
 
-function LikeProfile() {
+/* To be updated next time:
+    1. Search "This first item will be perfected next time"
+
+*/
+
+function ClickToLikeInterface () {
+    function ToLogin() {
+        if (className("ImageView").id("login").clickable().exists()) {
+            className("ImageView").id("login").clickable().findOnce().click();
+            sleep(2000); // Load after login
+        };
+    };
+    // Go to the like interface from homepage
+    function FromHomepageToLikeInterface() {
+        className("Button").desc("帐户及设置").clickable().findOne().click();
+        sleep(1000); // Delay 1s
+        click("DTEmiemie");
+        sleep(1000); // Delay 1s
+        className("RelativeLayout").descEndsWith("次赞").clickable().findOnce().click();
+    };
+
+    launch("com.tencent.mobileqq");
+    sleep(1000);
+    ToLogin();
+    // If the interface can click the avatar to call out the menu
+    if (className("Button").desc("帐户及设置").clickable().exists()) {
+        toast("帐户已登录，准备进入回赞页面");
+        FromHomepageToLikeInterface();
+    };
+
+};
+
+function Like() {
         // Number means counts of like to click. Default number is 20.
         // let number = arguments[0] ? arguments[0] : 20;
 
@@ -25,18 +57,65 @@ function LikeProfile() {
             sleep(4000);
             ScrollDownProfile();
             sleep(2000);
-        }
-    }
+        };
+    };
 
-    
-    
+
+    // ===variables===
     let WhetherToLike = "on";
+    // - Custom -
     let LikeNumber = 20;
 
+    // Start to a thread to determine if Likes Reach the Limit of Nearby People
+    let IfLimit = threads.start(function(){
+        auto();
+        function ExitScript(ExitWay){
+            // Ways[0]
+            if (ExitWay == "Just exit the script") {
+                exit();
+            }
+            
+            // This first item will be perfected next time
+
+            if (ExitWay == "Exit the script and QQ") {
+                function ClickButton(button) {
+                    button.findOnce.click()
+                    sleep(1000);
+                };
+
+                let TitleBtnLeft = className("TextView").id("ivTitleBtnLeft");
+                if (TitleBtnLeft.exists()) {
+                    ClickButton(TitleBtnLeft);
+                    if (TitleBtnLeft.exists()) {
+                        ClickButton(TitleBtnLeft);
+                    }
+                }
+            }
+        }
+
+
+        // Thread variables
+        Ways = ["Just exit the script","Exit script and QQ"];
+        ExitWay = Ways[0];
+        
+        // Thread Main
+        events.observeToast();
+        events.onToast(function(toast){
+            while (true) {
+                if (toast.getText() === "每天最多给50个附近的人点赞哦。") {
+                    console.log("Likes have been completed");
+                    ExitScript(ExitWay);
+                }
+            }
+        })
+    })
+
+    
+    // ===Main===
+    sleep(500); // To load the like interface (Optional)
     toast("The script is about to run, everyone will like [" + LikeNumber + "] points");
     sleep(3000);
-
-    while(WhetherToLike="on") {
+    while(WhetherToLike = "on") {
         for (let i = 0; i < LikeNumber; i++) {
             ClickLike();
             sleep(100);
@@ -44,18 +123,12 @@ function LikeProfile() {
         };
         ShowMore();
         ScrollDownProfile();
-    }
-
-    
+    };
+};
+ClickToLikeInterface();
+while (Like()) {
+    Like();
 }
-LikeProfile();
 
-/*
-events.observeToast();
-events.onToast(function(toast){
-        log("Toast内容: " + toast.getText() + " 包名: " + toast.getPackageName());
-    });
-// √ 每天最多给50个附近的人点赞
 
-*/
 
